d83780e997fb78709c7a64b194c5ab99100c9c6b
yeffon <exdsxt@gmail.com>
2018-01-09 01:36:21 -0500

    Created a register activity and prompt for user sign up info.

--- app/src/main/java/com/example/yeffo/studioproject/MainActivity.java
+++ app/src/main/java/com/example/yeffo/studioproject/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yeffo.studioproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username, password;
+        Button signIn, signUp;
+        final Intent reg_Act = new Intent(getApplicationContext(), Register.class);
+
+        username = (EditText) findViewById(R.id.userText);
+        password = (EditText) findViewById(R.id.pwText);
+        signIn = (Button) findViewById(R.id.signIn);
+        signUp = (Button) findViewById(R.id.register);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(reg_Act);
+            }
+        });
     }
 }
