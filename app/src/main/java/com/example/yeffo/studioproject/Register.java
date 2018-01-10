568964ebf7d306302c2f830be92a965f9a7c5102
yeffon <exdsxt@gmail.com>
2018-01-09 19:00:51 -0500

    Created to classes one for what a user is and another for storing a local user

--- app/src/main/java/com/example/yeffo/studioproject/Register.java
+++ app/src/main/java/com/example/yeffo/studioproject/Register.java
@@ -2,12 +2,31 @@ package com.example.yeffo.studioproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+
+    public EditText reg_user, reg_email, reg_pw, reg_confirm;
+    public Button reg_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        reg_user = (EditText) findViewById(R.id.registerUN);
+        reg_email = (EditText) findViewById(R.id.registerEmail);
+        reg_pw = (EditText) findViewById(R.id.registerPW);
+        reg_confirm = (EditText) findViewById(R.id.registerConfirm);
+        reg_signUp = (Button) findViewById(R.id.signUp);
+
+        reg_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
